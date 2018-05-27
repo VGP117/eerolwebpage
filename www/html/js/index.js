1015553a157e5c8367d8a821093d517e492d187c
VGP117 <eerolehtinen99@gmail.com>
2018-05-28 02:33:33 +0300

    asd

--- www/html/js/index.js
+++ www/html/js/index.js
@@ -15,6 +15,13 @@ $("#searchInput").on("change paste keyup", function () {
                 return false; // means break in jQuery each
             }
         });
-        
     });
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
