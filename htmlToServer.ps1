ed28baa1e0386f60375c71041da6b9ae63980277
VGP117 <eerolehtinen99@gmail.com>
2018-06-01 00:10:13 +0300

    swag-man changes

--- htmlToServer.ps1
+++ htmlToServer.ps1
@@ -4,4 +4,3 @@ gcloud compute scp --recurse E:\Documents\EeroLWebpage\html free-instance:~/temp
 Move-Item -Path $PSScriptRoot\images -Destination $PSScriptRoot\html
 Move-Item -Path $PSScriptRoot\downloads -Destination $PSScriptRoot\html
 gcloud compute ssh --zone us-east1-b free-instance --command 'sudo /var/move.sh'
-Pause
--- imagesToServer.ps1
+++ imagesToServer.ps1
@@ -1,3 +1,2 @@
 ﻿gcloud compute scp --recurse E:\Documents\EeroLWebpage\html\images free-instance:~/temp
 gcloud compute ssh --zone us-east1-b free-instance --command 'sudo /var/move.sh'
-Pause
