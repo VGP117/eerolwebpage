76df87bea024b38acbbc8f8d8a5c083f6fb923bf
VGP117 <eerolehtinen99@gmail.com>
2018-06-10 12:25:20 +0300

    added res end

--- eerolsite/server.js
+++ eerolsite/server.js
@@ -6,10 +6,12 @@ app.use(express.static("public"));
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
+    res.end();
 });
 
 app.get('/*', function (req, res) {
     res.send(404, "page " + req.path + " not found on lehtineneero.com");
+    res.end();
 });
 
 var server = app.listen(8080, function () {});
