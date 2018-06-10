48d56bde35d56b9d4b4959f80baf70b0925817ea
VGP117 <eerolehtinen99@gmail.com>
2018-06-10 12:13:59 +0300

    page not found message

--- eerolsite/server.js
+++ eerolsite/server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/*', function (req, res) {
-    res.send(404, req.path + "not found");
+    res.send(404, "page " + req.path + " not found on " + req.hostname);
 });
 
 var server = app.listen(8080, function () {});
