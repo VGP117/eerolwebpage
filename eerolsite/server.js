d8a18a0790db47daac45378ed83399f3c104a8af
VGP117 <eerolehtinen99@gmail.com>
2018-06-10 13:54:12 +0300

    no page found update

--- eerolsite/server.js
+++ eerolsite/server.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 var express = require('express');
 var app = express();
+app.disable("trust proxy");
 
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
-    res.end();
 });
 
 app.get('/*', function (req, res) {
-    res.send(404, "page " + req.path + " not found on lehtineneero.com");
-    res.end();
+    res.send(404, "page " + req.path + " not found on lehtineneero.com <br> <a href=\"lehtineneero.com\">Go to home page</a>");
 });
 
 var server = app.listen(8080, function () {});
