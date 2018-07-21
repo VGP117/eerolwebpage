df9b107c3b6ff566bf87d2cac956a6f9fab27361
VGP117 <eerolehtinen99@gmail.com>
2018-07-21 22:56:00 +0300

    new landing page and about me page added

--- eerolsite/private/js/index.js
+++ eerolsite/private/js/index.js
@@ -1,27 +1,7 @@
-$("#searchInput").on("change paste keyup", function () {
-    var inputVal = $(this).val().toLowerCase();
-
-    var li = $("#projectList").children();
-
-    li.each(function (index) {
-        var li = $(this);
-        var texts = li.find(".listItemTitle, .tag");
-        texts.each(function (index) {
-            if ($(this).html().toLowerCase().indexOf(inputVal) == -1) {
-                li.hide();
-            }
-            else {
-                li.show();
-                return false; // means break in jQuery each
-            }
-        });
-    });
-});
-
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+    }, 600);
 });
