cd95b83d07699cae3c59746e504ad277a5477351
VGP117 <eerolehtinen99@gmail.com>
2019-01-04 22:10:07 +0200

    fix icons ruining view when not loaded

--- lkpTimestampFetcher.ps1
+++ lkpTimestampFetcher.ps1
@@ -1,2 +1,3 @@
-﻿git -C "E:\Documents\Visual Studio 2017\Projects\lentokonepeli" tag -l --format='%(refname)   %(creatordate:short)'
+﻿$path = [Environment]::GetFolderPath("MyDocuments")
+git -C "$path\Visual Studio 2017\Projects\lentokonepeli" tag -l --format='%(refname)   %(creatordate:short)'
 Pause
