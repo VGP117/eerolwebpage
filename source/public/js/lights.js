8c415d132b73860f6edc7715a591db571c9b1d6f
Eero Lehtinen, JPR <eero@lehtineneero.com>
2019-03-05 18:03:26 +0200

    tag color made lighter

--- source/public/js/lights.js
+++ source/public/js/lights.js
@@ -1,5 +1,4 @@
 age = "max-age=4294967295;";
-b = document.body;
 
 if (is_l()) on();
 else off();
@@ -8,15 +7,14 @@ function is_l() {
     return document.cookie.includes("_l=1");
 }
 
-
 function off() {
     document.cookie = "_l=0;" + age;
-    b.classList.add("dark");
+    document.body.classList.add("dark");
 }
 
 function on() {
     document.cookie = "_l=1;" + age;
-    b.classList.remove("dark");
+    document.body.classList.remove("dark");
 }
 
 function t_l() {
