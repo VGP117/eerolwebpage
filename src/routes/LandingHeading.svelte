999aedcef17f22888ae1b8273b4b04d77c6a45df
Eero Lehtinen <eero@eerolehtinen.fi>
2024-03-09 16:32:20 +0200

    Fix images not being clickable

--- src/routes/LandingHeading.svelte
+++ src/routes/LandingHeading.svelte
@@ -1,23 +1,26 @@
 <script>
-	import mail from '$lib/images/mail.png';
-	import github from '$lib/images/github.png';
-	import linkedin from '$lib/images/linkedin.png';
+	import mail from "$lib/images/mail.png"
+	import github from "$lib/images/github.png"
+	import linkedin from "$lib/images/linkedin.png"
 </script>
 
 <section class="flex flex-col items-center gap-4">
 	<h1 class="mb-4 mt-12 text-center text-6xl font-bold">Eero Lehtinen</h1>
-	<div class="flex items-center gap-1">
-		<img src={mail} alt="email" class="mt-0.5 max-h-5" />
-		<a href="mailto:eero@eerolehtinen.fi">eero@eerolehtinen.fi</a>
-	</div>
-	<div class="flex gap-4">
-		<div class="flex items-center gap-1">
-			<img src={github} alt="Github" class="mt-0.5 max-h-5" />
-			<a href="https://github.com/eero-lehtinen">Github</a>
-		</div>
-		<div class="flex items-center gap-1">
-			<img src={linkedin} alt="LinkedIn" class="mt-0.5 max-h-5" />
-			<a href="https://www.linkedin.com/in/eero-lehtinen/">LinkedIn</a>
-		</div>
+	<a class="flex items-center gap-2" href="mailto:eero@eerolehtinen.fi">
+		<img src={mail} alt="Email" width="24" height="24" />
+		eero@eerolehtinen.fi
+	</a>
+	<div class="flex gap-5">
+		<a class="flex items-center gap-2" href="https://github.com/eero-lehtinen">
+			<img src={github} alt="Github" width="24" height="24" />
+			<span>Github</span>
+		</a>
+		<a
+			class="flex items-center gap-2"
+			href="https://www.linkedin.com/in/eero-lehtinen/"
+		>
+			<img src={linkedin} alt="LinkedIn" width="24" height="24" />
+			<span>LinkedIn</span>
+		</a>
 	</div>
 </section>
